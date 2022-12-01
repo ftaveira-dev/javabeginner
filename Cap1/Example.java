4ddc5b096e2299630632006871b4f36ceaf9f12c
Francisco Taveira <ftaveira.dev@gmail.com>
2022-12-01 19:45:00 -0300

    first_commit_Example1

---
+++ Cap1/Example.java
@@ -0,0 +1,13 @@
+package Cap1;
+
+/*
+Este é um programa Java Simples
+*/
+public class Example {
+
+    // Um programa Java começa com uma chamada a main().
+
+    public static void main(String args[]) {
+         System.out.println("Java drives the web");
+    }
+}
