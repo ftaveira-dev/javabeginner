804abc1b30f0e63edc086b9a5435553176992c5d
Francisco Taveira <ftaveira.dev@gmail.com>
2022-12-01 20:13:05 -0300

    example 2

---
+++ Cap1/Example2.java
@@ -0,0 +1,18 @@
+package Cap1;
+/*
+Este codigo demonstra uma variavel.
+ */
+public class Example2 {
+    public static void main(String args[]) {
+        int var1; //esta instrução declara uma variavel.
+        int var2; //esta instrução declara uma segunda variavel.
+
+        var1 = 1024; //esta instrução atribui um valor a primeira variavel.
+
+        System.out.println("var 1 contains" + var1);
+
+        var2 = var1/2;
+        System.out.print("var2 contains var1/2: ");
+        System.out.println(var2);
+        }
+}
