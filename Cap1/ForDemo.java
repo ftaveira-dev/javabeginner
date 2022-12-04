d6e0ed5c8c98e98e988f3927e93706f6c64cdad2
Francisco Taveira <ftaveira.dev@gmail.com>
2022-12-03 22:23:31 -0300

    demonstrando o laço for

---
+++ Cap1/ForDemo.java
@@ -0,0 +1,15 @@
+package Cap1;
+/*
+Este programa ira demonstrar o uso do FOR
+ */
+public class ForDemo {
+
+    public static void main(String args[]) {
+        int count;
+
+        for(count = 0; count < 5; count++ ) //esse laco itera 5vezes
+            System.out.println("this is count: " + count);
+
+        System.out.println("done!");
+    }
+}
