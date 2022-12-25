c16ee4043bae066fede83930d540fc7d4a2bbcd2
ftaveira-dev <ftaveira.dev@gmail.com>
2022-12-25 20:20:19 -0300

    tente isto 2-2 tabela verdade

---
+++ Cap2/LogicalOpTable.java
@@ -0,0 +1,33 @@
+package Cap2;
+/*
+Neste projeto sera criado um programa para exibir a tabela verdade dos operadores logicos, sera utilizada varios
+recursos do capitulo 2.
+
+Tente isto 2-2
+ */
+public class LogicalOpTable {
+    public static void main(String[] args) {
+        boolean p, q;
+        System.out.println("P\t\tQ\t\tAND\t\tOR\t\tXOR\t\tNOT");
+
+        p = true; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = true; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = true;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+
+        p = false; q = false;
+        System.out.print(p + "\t" + q + "\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (!p));
+    }
+}
