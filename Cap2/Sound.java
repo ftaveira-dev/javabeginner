8be5cf1e103b17f66c0e2704d69345a9047e7c0d
ftaveira-dev <ftaveira.dev@gmail.com>
2022-12-24 21:56:44 -0300

    Adicionado exercicio Sound

---
+++ Cap2/Sound.java
@@ -0,0 +1,19 @@
+package Cap2;
+/*
+Tente isto 2-1
+Calcule a distancia da queda de um raio cujo som leva 7,2 segundos para alcança-lo
+ */
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 7.2*1100;
+
+        System.out.println("The lightning is " + dist + "feer away.");
+
+        var distSom = 8.5/2;
+        System.out.println(distSom);
+
+
+    }
+}
