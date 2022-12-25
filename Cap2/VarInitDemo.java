c7bf7c30407893ba45c4a479bd7945c5d1940281
ftaveira-dev <ftaveira.dev@gmail.com>
2022-12-24 23:38:08 -0300

    Adicionado exemplo VarInitDemo

---
+++ Cap2/VarInitDemo.java
@@ -0,0 +1,14 @@
+package Cap2;
+
+public class VarInitDemo {
+    public static void main(String[] args) {
+        int x;
+        for(x = 0; x < 3; x++) {
+
+            int y = -1; // y será inicializada sempre que entrarmos no bloco
+            System.out.println("y is: " + y); //essa linha sempre exibe -1
+            y = 100;
+            System.out.println(" y is now: " + y);
+        }
+    }
+}
